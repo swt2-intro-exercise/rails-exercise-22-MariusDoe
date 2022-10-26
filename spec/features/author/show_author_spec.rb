f31496b85f895fdaabd444e6710842c775defd2a
Marius Dörbandt <github.marius@doerbandt.de>
2022-10-26 15:07:34 +0200

    Fix #7

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -5,4 +5,12 @@ describe "Show author page", type: :feature do
     @alan = FactoryBot.create :author
     visit author_path(@alan)
   end
+
+  it "renders an author's details" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+  end
 end
