5df868e2de1000bc4e194bf898ea4b75add818fe
Marius Dörbandt <github.marius@doerbandt.de>
2022-10-31 18:11:42 +0100

    Fix #18

--- spec/features/paper/papers_index_spec.rb
+++ spec/features/paper/papers_index_spec.rb
@@ -13,4 +13,12 @@ describe "Papers index page" do
         visit papers_path
         expect(page).to have_link nil, href: edit_paper_path(paper)
     end
+
+    it "contains delete links" do
+        paper = Paper.create! valid_attributes
+        count = Paper.count
+        visit papers_path
+        find("a[data-turbo-method='delete']").click
+        expect(Paper.count).to be < count
+    end
 end
