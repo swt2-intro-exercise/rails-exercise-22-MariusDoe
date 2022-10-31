d1dcc5948371e6f8b569a3d2e5f6fc15198626b8
Marius Dörbandt <github.marius@doerbandt.de>
2022-10-31 17:59:52 +0100

    Fix #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,12 @@
 class Paper < ApplicationRecord
+    validates :title,
+        presence: true,
+        length: { minimum: 1 }
+
+    validates :venue,
+        presence: true
+
+    validates :year,
+        presence: true,
+        numericality: true
 end
